88606a9e98505282c135bade9839e70b07201d8d
Marius Baden <93079021+mabdn@users.noreply.github.com>
2024-08-31 09:12:16 +0200

    Improve wording

--- code_is_private.cpp
+++ code_is_private.cpp
@@ -1,6 +1,6 @@
 // The code I wrote in this project is private. 
 // It is actively used by the KA-RaceIng team.
-// It is the core of path planning at KA-RaceIng and is essential for the team's success in autonomous racing. 
+// It is the core of path planning at KA-RaceIng and essential for our success in autonomous racing. 
 // Hence, I cannot publish any of this code here. 
 
-// I can give you more insights and show you code in a personal discussion, though.
+// I can give you more insights in a personal discussion, though.
